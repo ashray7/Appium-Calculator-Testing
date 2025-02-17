f1e09373b821908ae9f21ca1cabe95050ff6459d
Ashray Baral <ashraybaral77@gmail.com>
2025-02-17 16:55:46 +0545

    div by zero

--- calculator.py
+++ calculator.py
@@ -29,11 +29,16 @@ def driver(request):
 
 @pytest.mark.usefixtures("driver")
 class TestCalculator:
+
+    def click_clear(self):
+        """Helper method to click the clear button"""
+        btn_clear = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_c_s")
+        btn_clear.click()
     
-    def test_1addition(self):
+    def test_1_addition(self):
         """Test case for 2 + 3 = 5"""
         self.driver.tap([(500, 500)])
-
+        self.click_clear()
         btn_2 = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_2_s")
         btn_plus = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_plus_s")
         btn_3 = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_3_s")
@@ -47,17 +52,17 @@ class TestCalculator:
 
         # Try to get the result
         result = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/result").text.strip()
+        # print(" ")
         print(f"The result of 2 + 3 {result}")
 
         # Assertion
         assert result == "= 5"
 
 
-    def test_2subtraction(self):
+    def test_2_subtraction(self):
         """Test case for 5 - 3 = 2"""
 
-        btn_clear = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_c_s")
-        btn_clear.click()
+        self.click_clear()
 
         btn_5 = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_5_s")
         btn_minus = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_minus_s")
@@ -78,11 +83,10 @@ class TestCalculator:
         assert result == "= 2"
 
 
-    def test_3multiplication(self):
+    def test_3_multiplication(self):
         """Test case for 2 * 3 = 6"""
 
-        btn_clear = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_c_s")
-        btn_clear.click()
+        self.click_clear()
 
         btn_2 = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_2_s")
         btn_mul = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_mul_s")
@@ -103,11 +107,10 @@ class TestCalculator:
         assert result == "= 6"
 
 
-    def test_4div(self):
+    def test_4_div(self):
         """Test case for 6 / 2 = 3"""
 
-        btn_clear = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_c_s")
-        btn_clear.click()
+        self.click_clear()
 
         btn_6 = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_6_s")
         btn_div = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_div_s")
@@ -126,6 +129,29 @@ class TestCalculator:
 
         # Assertion
         assert result == "= 3"
+
+    def test_5_Div_By_Zero(self):
+        """Test case for 5 / 0 """
+
+        self.click_clear()
+
+        btn_5 = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_5_s")
+        btn_div = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_div_s")
+        btn_0 = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_0_s")
+        btn_equals = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/btn_equal_s")
+
+        # Click the buttons
+        btn_5.click()
+        btn_div.click()
+        btn_0.click()
+        btn_equals.click()
+
+        # Try to get the result
+        result = self.driver.find_element(by=AppiumBy.ID, value="com.miui.calculator:id/result").text.strip()
+        print(f"The result of 5 / 0 {result}")
+
+        # Assertion
+        assert result == "= Can't divide by zero"
 
 
 
